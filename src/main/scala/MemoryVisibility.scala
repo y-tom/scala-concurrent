d518660c8da67ada5cbca12610a541e3c4d6eb54
y-tom <114702943+y-tom@users.noreply.github.com>
2026-04-26 11:57:41 +0900

    メモリの可視性と逸出 synchronized

--- src/main/scala/MemoryVisibility.scala
+++ src/main/scala/MemoryVisibility.scala
@@ -3,20 +3,26 @@
 // readyがtrueになったらnumberの値を出力する
 // スレッド間で同期が取れていないため、正しく動くときも正しく動かないときもある
 object MemoryVisibility  extends App{
-  // --- volatileで可視性を保証する ---
-  @volatile var number = 0
-  @volatile var ready = false
+  // --- synchronizedで可視性とアトミック性を保証する ---
+  var number = 0
+  var ready = false
+  // 読み取り側を同期
+  private[this] def getNumber: Int = synchronized{number}
+  private[this] def getReady: Boolean = synchronized{ready}
 
   // === 別スレッド ===
   new Thread (() => {
-    while (!ready) {
+    while (!getReady) {
       // Thread.yieldメソッド
       // 他の実行可能なスレッドがある場合は実行、ない場合は待つ
       Thread.`yield`()
     }
-    println(number)
+    println(getNumber)
   }).start()
 
-  number = 2525
-  ready = true
+  // 書き込み側を同期
+  synchronized{
+    number = 2525
+    ready = true
+  }
 }
